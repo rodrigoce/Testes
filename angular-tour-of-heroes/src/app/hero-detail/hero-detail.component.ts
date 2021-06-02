7af49f26d2a9c898b14febbbe04aad539fc6ef38
Rodrigo Castro Eleotério <rodrigo.ce@gmail.com>
2021-06-02 14:15:04 -0300

    heroes project

---
+++ angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,41 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
+})
+export class HeroDetailComponent implements OnInit {
+
+  /*@Input()*/ hero?: Hero;
+
+  constructor(private heroService: HeroService, 
+    private route: ActivatedRoute,
+    private location: Location) { }
+
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  }
+
+  save(): void {
+    if (this.hero) {
+      this.heroService.updateHero(this.hero)
+        .subscribe(() => this.goBack());
+    }
+  }
+  
+  goBack(): void {
+    this.location.back();
+  }
+}
